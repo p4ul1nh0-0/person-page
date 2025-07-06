4cff9c3c4e1bf660ae68c726a97628a35d8a74a4
p4ul1nh0-0 <joaopaulo.nane@gmail.com>
2025-07-05 23:48:13 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.querySelector("#mensagem-btn");
-const mensagem = document.querySelector("#mensagem");
-
-botao.addEventListener("click", () => {
-  mensagem.textContent = "Bem-vindo à minha página! Que bom ter você aqui. 😊";
-});
